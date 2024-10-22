485c64a9ecd691cd2b8353909358225445efce7e
Onur45500 <onur.akmese@viacesi.fr>
2024-10-22 15:43:17 +0200

    Initialized the project

---
+++ csharp-asp-net-core-mvc-menu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csharp_asp_net_core_mvc_menu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
