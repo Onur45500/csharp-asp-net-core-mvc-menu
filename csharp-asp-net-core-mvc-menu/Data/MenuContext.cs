9899af5cc96a641aee10ce1bc599ff962e6676e9
Onur45500 <onur.akmese@viacesi.fr>
2024-10-23 16:14:26 +0200

    Mise à jour du projet et ajout de MenuContext

---
+++ csharp-asp-net-core-mvc-menu/Data/MenuContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace csharp_asp_net_core_mvc_menu.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext( DbContextOptions<MenuContext> options ) : base(options)
+        {
+
+        }
+    }
+}
