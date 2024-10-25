f4d082d382664fc3a1bad7b4edc574ff81ec50ed
onur.akmese <onur.akmese@lhs-sa.ch>
2024-10-25 16:32:12 +0200

    Ajout de configurations de relations dans MenuContext
    Ajout de l'importation du namespace `csharp_asp_net_core_mvc_menu.Models`.
    Ajout de la méthode `OnModelCreating` dans `MenuContext` pour configurer les relations entre `DishIngredient`, `Dish`, et `Ingredient`.
    Configuration de la clé primaire composite pour `DishIngredient` avec `DishId` et `IngredientId`.
    Configuration des relations avec clés étrangères entre `DishIngredient` et `Dish`, et entre `DishIngredient` et `Ingredient`.
    Appel de la méthode de base `OnModelCreating` à la fin de la méthode surchargée.

--- csharp-asp-net-core-mvc-menu/Data/MenuContext.cs
+++ csharp-asp-net-core-mvc-menu/Data/MenuContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using csharp_asp_net_core_mvc_menu.Models;
+using Microsoft.EntityFrameworkCore;
 namespace csharp_asp_net_core_mvc_menu.Data
 {
     public class MenuContext : DbContext
@@ -6,6 +7,22 @@ namespace csharp_asp_net_core_mvc_menu.Data
         public MenuContext( DbContextOptions<MenuContext> options ) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new { di.DishId, di.IngredientId });
+
+            modelBuilder.Entity<DishIngredient>()
+                          .HasOne(di => di.Dish)
+                          .WithMany(d => d.DishIngredients)
+                          .HasForeignKey(di => di.DishId);
+
+            modelBuilder.Entity<DishIngredient>().HasOne(di => di.Ingredient)
+                        .WithMany(i => i.DishIngredients)
+                        .HasForeignKey(di => di.IngredientId);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
